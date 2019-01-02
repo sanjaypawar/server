d23130979ad1d24cfc930cc2709dc2c61e6d763f
sanjaypawar <sanjaybpawar@gmail.com>
2019-01-02 18:50:06 +0530

    some changes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// videos 28 complete... 29 starting...
+
 const express=require('express');
+require('./services/passport');
+
+
+
 const app=express();
+    
+require('./routes/authRoutes')(app);
 
-app.get('/', (req, res) =>{
- res.send({hi:'buddy'});
-});
-
-
+   
 const PORT=process.env.PORT || 5000;
 app.listen(PORT);
